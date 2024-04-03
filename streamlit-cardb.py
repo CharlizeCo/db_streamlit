2c73cc10abf866878ef8d69bfd48eab284c062f5
CharlizeCo <150076368+CharlizeCo@users.noreply.github.com>
2024-04-03 15:51:30 -0400

    Update streamlit-cardb.py

--- streamlit-cardb.py
+++ streamlit-cardb.py
@@ -18,7 +18,7 @@ def connect_to_db():
     mydb = mysql.connector.connect(
       host="localhost",
       user="root",
-      port = 3306  
+      port = 3306,  
       password= pw,
       database="cardb"
     )
